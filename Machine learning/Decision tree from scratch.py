7416c7c23ef71c9f6498da0a88a3e3bd35f6ed35
Mobenchi <benaich45@gmail.com>
2023-03-31 05:13:12 +0000

    Version 1.3

--- Machine learning/Decision tree from scratch.py
+++ Machine learning/Decision tree from scratch.py
@@ -104,6 +104,14 @@ class DecisionTree:
         value = counter.most_common(1)[0][0]
         return value
 
-    def predict():
-
+    def predict(self, X):
+        return np.array([self._traverse_tree(x, self.root) for x in X])
+    
+    def _traverse_tree(self, x, node):
+        if node.is_leaf_node():
+            return node.value()
+        
+        if x[node.feature] <= node.threshold:
+            return self._traverse_tree(x, node.left)
+        return self._traverse_tree(x, node.right)
         
